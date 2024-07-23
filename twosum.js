6a735f604528c0abc0e42654edf8cd9480c3df59
Mishaj__k <amstig100@gmail.com>
2024-07-23 20:06:04 +0530

    twoSum 23/07/24

---
+++ twosum.js
@@ -0,0 +1,22 @@
+function twoSum(arr, target) {
+    let hashMap = {};
+
+    for (let i = 0; i < arr.length; i++) {
+        let complement = target - arr[i];
+
+        if (hashMap.hasOwnProperty(complement)) {
+            return { firstIndex: hashMap[complement], secondIndex: i };
+        }
+
+        hashMap[arr[i]] = i;
+    }
+
+    return null; // Return null if no such pair is found
+}
+
+console.log(twoSum([2, 7, 11, 15], 9)); // Prints { firstIndex: 0, secondIndex: 1 }
+
+
+
+
+
